ec512c30a0eb86470805e4e7eb22c87a2cc66cd2
jayden <46307433+krmeet@users.noreply.github.com>
2023-05-06 14:02:20 +0100

    intro fixes (probably removing later)

--- scripts/menu/loading/Intro.gd
+++ scripts/menu/loading/Intro.gd
@@ -8,18 +8,18 @@ var can_skip = false
 var can_skip2 = false
 
 func _ready():
-	
+
 	# fix hands
 	$Avatar/ArmL/Pointer.position = Vector3(0,0,0)
 	$Avatar/ArmR/Mesh.position = Vector3(0,0,0)
-	
+
 	$ColorRect.modulate.a = 1
 #	get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (true) else Window.MODE_WINDOWED
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	await get_tree().create_timer(1).timeout
 	running = true
 	$Avatar/Animations.play("Float")
-	$Avatar/Animations.playback_speed = 1
+	$Avatar/Animations.speed_scale = 1
 	$Camera2.rotation_degrees.y = 0
 	await get_tree().create_timer(1).timeout
 	$AudioStreamPlayer.play()
@@ -39,11 +39,11 @@ func _input(event):
 		get_tree().change_scene_to_file("res://scenes/Init.tscn")
 
 func _process(delta):
-	
+
 	$Sprite3D.rotation_degrees.z += 16 * delta
 	$Sprite3D2.rotation_degrees.z += 8 * delta
 	$Sprite3D3.rotation_degrees.z += 4 * delta
-	
+
 	if running and not fading:
 		$ColorRect.modulate.a += (0 - $ColorRect.modulate.a) * 0.4 * delta
 	if panning:
