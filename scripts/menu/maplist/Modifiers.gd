91a7fefa65f07eee858f40dd1a526e0d184028db
anthony-63 <back.up.email.158@gmail.com>
2023-10-21 20:58:24 -0400

    Add nofail mod button

--- scripts/menu/maplist/Modifiers.gd
+++ scripts/menu/maplist/Modifiers.gd
@@ -3,9 +3,13 @@ extends Control
 func _ready():
 	$Reset.pressed.connect(_on_reset_modifiers)
 	$Speed/SpeedSpinbox.value_changed.connect(_set_speed)
+	$Grid/NofailButton.toggled.connect(_toggle_nofail)
 	
+	$Grid/NofailButton.button_pressed = Rhythia.selected_mods.no_fail
+	
+func _toggle_nofail(pressed):
+	Rhythia.selected_mods.no_fail = pressed
 func _set_speed(value):
-	print("Changed speed to: ", value);
 	Rhythia.selected_mods.speed_custom = value
 
 func _on_reset_modifiers():
