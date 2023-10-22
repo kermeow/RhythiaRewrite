4c498be51bd1f923a142e26931d85f6240619aca
anthony-63 <back.up.email.158@gmail.com>
2023-10-21 20:44:28 -0400

    Add speed modifier, very crude

--- scripts/game/Mods.gd
+++ scripts/game/Mods.gd
@@ -14,7 +14,7 @@ const SpeedPresets = [
 	1.25,
 	1.35
 ]
-var speed_mod = Speed.PRESET
+var speed_mod = Speed.CUSTOM
 var speed_preset = 3
 var speed_custom = 1
 var speed:float:
--- scripts/menu/maplist/Modifiers.gd
+++ scripts/menu/maplist/Modifiers.gd
@@ -2,6 +2,11 @@ extends Control
 
 func _ready():
 	$Reset.pressed.connect(_on_reset_modifiers)
+	$Speed/SpeedSpinbox.value_changed.connect(_set_speed)
+	
+func _set_speed(value):
+	print("Changed speed to: ", value);
+	Rhythia.selected_mods.speed_custom = value
 
 func _on_reset_modifiers():
 	pass
