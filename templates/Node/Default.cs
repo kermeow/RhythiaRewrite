ee412cba79c30d28e3c15e2e3425e10cb8e8b537
jayden <46307433+krmeet@users.noreply.github.com>
2023-12-01 21:24:51 +0000

    make c# project

---
+++ templates/Node/Default.cs
@@ -0,0 +1,19 @@
+// meta-name: Default C#
+// meta-description: Default C# script including namespace
+// meta-default: true
+using _BINDINGS_NAMESPACE_;
+
+namespace Rhythia.Game;
+
+public partial class _CLASS_ : _BASE_
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
