26a2e1d5211159cc186de894d4108090002f67f3
jayden <46307433+krmeet@users.noreply.github.com>
2024-02-20 20:32:09 +0000

    move stuff

--- Rhythia.Core/Maps/MapSet.cs
+++ Rhythia.Core/Maps/MapSet.cs
@@ -4,6 +4,7 @@ public class MapSet : IMapSet
 {
     public MapSetMetadata Metadata { get; set; }
     public List<IMap> Maps { get; set; } = new();
+    public string Path { get; set; } = string.Empty;
     public List<string> Files { get; set; } = new();
     public MapSet()
     {
